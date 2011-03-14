60f9237a42316d90dbecd769ab75867937378971
Alex Belanger <i.caught.air@gmail.com>
2011-03-14 17:31:48 -0400

    Started a GameNgine server project (Hello world style).

---
+++ Server/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define ACE_DOESNT_DEFINE_MAIN
+#include "ace/ace.h"
+
+/* ==== TODO list ====
+- Add linux support for ACE_wrappers
+====================== */
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
